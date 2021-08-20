918c3def6bcf1d1b509e6ab75f33a6ed9708b962
Rodrigo Lourenço <rodrigo.lourenco@blendit.com.br>
2021-08-20 20:49:29 -0300

    Variaveis e operadores

---
+++ basico/myScriptBasico.js
@@ -0,0 +1,73 @@
+//alert('Iniciando os Trabalhos');
+console.log('Iniciando os Trabalhos');
+document.getElementById('my-app').innerHTML = 'Iniciando os Trabalhos';
+
+/**
+ * VARIAVEIS
+ */
+
+var nome = "Xuxa";
+var idadeXuxa = 58;
+var preco = 125.37;
+var statusVenda = true;
+
+console.log('idadeXuxa: ', idadeXuxa)
+idadeXuxa = 'cinquenta e oito anos';
+console.log('idadeXuxa: ', idadeXuxa)
+
+/**
+ * OPERADORES
+ * 
+ * Aritmético: + - * / %
+ * Atribuição: = += -= *= /= %= ++ --
+ * Relacional: == != < <= > >=
+ * Lógico: && ||
+ */
+
+/**
+ * Aritmético
+ */
+
+console.log('Adição - 5 + 5: ', 5 + 5);
+console.log('Divisão - 15 / 3: ', 15 / 3);
+console.log('Resto da Divisão - 15 % 3: ', 15 % 3);
+
+/**
+ * Atribuição
+ */
+
+var soma = 12 + 3;
+console.log("Soma:" , soma)
+soma = soma + 100;
+console.log("Soma:" , soma)
+soma += 200;
+console.log("Soma:" , soma)
+soma++
+console.log("Soma:" , soma)
+
+/**
+ * Relacional == != < <= > >=
+ */
+
+var eIgual = 5 == 5;
+console.log("eIgual - 5 == 5:" , eIgual)
+
+var eDiferente = 5 != 5;
+console.log("eDiferente -5 != 5:" , eDiferente)
+
+var eMaior = 5 > 5;
+console.log("eMaior-  5 > 5:" , eMaior)
+
+var eMenorOuIgual = 5 <= 5;
+console.log("eMenorOuIgual - 5 <= 5:" , eMenorOuIgual)
+
+/**
+ * Lógico: && ||
+ */
+
+var eh = true && false;
+console.log("eh - true && false:" , eh)
+
+var ou = 5 == 5 || false;
+console.log("ou - 5 == 5 || false:" , ou)
+
