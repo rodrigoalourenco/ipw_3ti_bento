52d4b24052677ebdad0849397c3bfa76ee9d8838
Rodrigo de Andrade Lourenço <rodrigoalourenco@hotmail.com>
2021-10-08 19:57:57 -0300

    Eventos

--- 07-dom/myScriptDom.js
+++ 07-dom/myScriptDom.js
@@ -2,3 +2,8 @@ console.log("DOM");
 var elemento = document
     .getElementById("conteudo");
 console.log(elemento);
+
+document.getElementById("dados").innerHTML = "IPW - Internet e progamação Web";
+
+var element = document.getElementById("nome").value;
+console.log(element)
---
+++ 08 - eventos/myScripEventos.js
@@ -0,0 +1,6 @@
+var elemento = document.getElementById('enviar');
+elemento.onclick = function() {
+    var valor = document
+    .getElementById('nome').value;
+    console.log(valor);
+}
